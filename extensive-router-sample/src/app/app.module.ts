b3da3c9183b2cfd94969fd614658fa0c98d946f2
Evelyn Pei <pei.evelyn.s@gmail.com>
2020-12-08 15:59:07 -0800

    Refactored and moved routing separate from app module

--- extensive-router-sample/src/app/app.module.ts
+++ extensive-router-sample/src/app/app.module.ts
@@ -1,20 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
 import { HeroListComponent } from './hero-list/hero-list.component';
 import { CrisisListComponent } from './crisis-list/crisis-list.component';
-import { AppRoutingModule } from './app-routing.module';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const appRoutes: Routes = [
-  { path: '', redirectTo: '/heroes', pathMatch: 'full' },
-  { path: 'crisis-center', component: CrisisListComponent },
-  { path: 'heroes', component: HeroListComponent },
-  { path: '**', component: PageNotFoundComponent }
-];
+
 
 @NgModule({
   declarations: [
@@ -26,13 +21,8 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(
-      appRoutes,
-      { enableTracing: true }
-    ),
     AppRoutingModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
