81799bcb9162078884523951743a2820fb8ef268
Evelyn Pei <pei.evelyn.s@gmail.com>
2020-12-08 14:11:40 -0800

    Added routes to app module

--- angular-router-sample/src/app/app.module.ts
+++ angular-router-sample/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CrisisListComponent } from './crisis-list/crisis-list.component';
 import { HeroesListComponent } from './heroes-list/heroes-list.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,11 @@ import { HeroesListComponent } from './heroes-list/heroes-list.component';
     HeroesListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: 'crisis-list', component: CrisisListComponent },
+      { path: 'heroes-list', component: HeroesListComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
