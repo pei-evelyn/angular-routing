8b851b579a0b2e58167db87133344a66b9db22e5
Evelyn Pei <pei.evelyn.s@gmail.com>
2020-12-09 14:32:14 -0800

    Added separate routing for heroes

--- extensive-router-sample/src/app/app.module.ts
+++ extensive-router-sample/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
-import { HeroListComponent } from './hero-list/hero-list.component';
 import { CrisisListComponent } from './crisis-list/crisis-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { HeroesModule } from './heroes/heroes.module';
@@ -15,15 +14,14 @@ import { HeroesModule } from './heroes/heroes.module';
 @NgModule({
   declarations: [
     AppComponent,
-    HeroListComponent,
     CrisisListComponent,
     PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    HeroesModule,
     AppRoutingModule,
-    HeroesModule
   ],
   bootstrap: [AppComponent]
 })
---
+++ extensive-router-sample/src/app/heroes/hero-detail/hero-detail.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Hero } from '../hero';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+
+import { switchMap } from 'rxjs/operators';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent implements OnInit {
+
+  @Input() hero: Hero;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private service: HeroService
+  ) { }
+
+  ngOnInit() {
+    this.hero$ = this.route.paramMap.pipe(
+      switchMap((params: ParamMap) =>
+        this.service.getHeroes(params.get('id')))
+    );
+  }
+
+}
